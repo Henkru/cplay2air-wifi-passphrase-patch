b3a10ce827335e9b762484931bb6a563923ee6b9
Henri Nurmi <henri@nurmi.me>
2021-07-06 17:04:13 +0300

    Add PATH

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-
+export PATH=$PWD/toolchain/gcc-linaro-5.1-2015.08-x86_64_arm-linux-gnueabi/bin:$PATH
+ls .
+echo $PATH
+echo $PWD
+ls $PWD/toolchain/gcc-linaro-5.1-2015.08-x86_64_arm-linux-gnueabi/bin
 arm-linux-gnueabi-gcc -Wall -shared -g -fPIC -o inject.o inject.c && arm-linux-gnueabi-strip inject.o
